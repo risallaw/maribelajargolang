2722abd7c3fd21b0e6d62d23cd839b85811bb88d
risallaw <laurentius.risal@it.maranatha.edu>
2019-05-02 13:59:06 +0700

    +04_ArraySlice::04_MemotongArraySlice::01

--- 04_ArraySlice/04_MemotongArraySlice/01/main.go
+++ 04_ArraySlice/04_MemotongArraySlice/01/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var deret = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	// [0 1 2 3 4 5 6 7 8 9]
+	fmt.Println(deret)
+
+	// [2 3 4 5 6 7 8 9]
+	fmt.Println(deret[2:])
+
+	// [0 1 2 3 4 5]
+	fmt.Println(deret[:6])
+
+	// [2 3 4 5]
+	fmt.Println(deret[2:6])
 }
