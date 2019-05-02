01d661f8d7c693af8461353a9851aeec1d3a170a
risallaw <laurentius.risal@it.maranatha.edu>
2019-05-02 11:41:45 +0700

    +04_ArraySlice::03_Slice::02

--- 04_ArraySlice/03_Slice/02/main.go
+++ 04_ArraySlice/03_Slice/02/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	var count int
+	daftarNilai := []float64{}
+	for {
+		var nilaiStr string
+		fmt.Print("Masukan nilai (x - berhenti): ")
+		fmt.Scanln(&nilaiStr)
+		if nilaiStr == "x" {
+			break
+		}
+
+		nilai, err := strconv.ParseFloat(nilaiStr, 64)
+		if err != nil {
+			fmt.Println("Masukan tidak sesuai, silahkan coba lagi.")
+		} else {
+			daftarNilai = append(daftarNilai, nilai)
+			count++
+		}
+	}
+
+	fmt.Println(daftarNilai)
+	var total float64
+	for _, n := range daftarNilai {
+		total += n
+	}
+
+	rataRata := total / float64(count)
+	fmt.Printf("\nRata-rata nilai = %f\n", rataRata)
 }
